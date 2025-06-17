ae61ef7064ed865ce2846044a993445b6a700d1b
Miguel Attie <attiemiguel751@gmail.com>
2025-06-17 00:08:48 -0300

    base project with `next.js`,`react and react-dom` and `tailwind`

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import './globals.css';
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 class="text-laland" >Welcome to the Home Page</h1>
+    </div>
+  );
+}
+export default Home;
