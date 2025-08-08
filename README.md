81153dcc3c2561023f304ec74ee570cdf9c48ce6
Miguel Attie <attiemiguel751@gmail.com>
2025-08-08 00:55:05 -0300

    ✨feat: `inicialize-application`

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trabalho de Conclusão de Curso (T.C.C.)
+
+## [www.recipay.com.br](https://www.recipay.com.br)
+
+### Futuras implementações de sessionId e Dashboard para o usúario
+
+# _Miguel A. Pereira_
---
+++ infra/database.js
@@ -0,0 +1,27 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Database query error:", error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
