717d30b2af6fdb95fb4a7cff9154c865bbd6dee1
Miguel Attie <attiemiguel751@gmail.com>
2025-08-13 08:35:36 -0300

    feat: add `pg-module`, `node-pg-migrate`; `dotenv` and create `database.ts`

---
+++ infra/database.ts
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+
+async function query(queryObject: string) {
+  const client = new Client({
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT ? Number(process.env.PGPORT) : undefined,
+    database: process.env.PGDATABASE,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
